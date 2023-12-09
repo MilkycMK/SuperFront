b5b1ba9a363adb20722d3a32e068daafb354ab62
MilkycMK <123307475+MilkycMK@users.noreply.github.com>
2023-12-09 23:06:43 +0300

    s

---
+++ src/main/java/net/mlk/adolffront/screens/login/LoginRegisterScreen.java
@@ -0,0 +1,12 @@
+package net.mlk.adolffront.screens;
+
+import javafx.scene.layout.BorderPane;
+
+public class LoginRegisterScreen extends BorderPane {
+
+    
+    public LoginRegisterScreen() {
+
+    }
+
+}
---
+++ src/main/java/net/mlk/adolffront/utils/FontUtils.java
@@ -0,0 +1,2 @@
+package net.mlk.adolffront.utils;public class FontUtils {
+}
